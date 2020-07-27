7e3843f0efa2c1142b0b38ff53b3344ed8427801
Aaron Hayes <aaronhayes@users.noreply.github.com>
2020-07-27 12:51:18 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# cuid generator
+
+Simple app to generate [https://github.com/ericelliott/cuid](cuids) for development and testing.
+
+Use these cuids at your own risk.
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/zeit/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,52 @@
-import Head from 'next/head'
+import { useState } from "react";
+import Head from "next/head";
+
+import newCuid from "cuid";
 
 export default function Home() {
+  const [slug, setSlug] = useState(false);
+  const [cuid, setCuid] = useState(newCuid());
+
+  const refreshCuid = () => {
+    if (slug) {
+      setCuid(newCuid.slug());
+    } else {
+      setCuid(newCuid());
+    }
+  };
+
+  const toggleSlug = () => {
+    if (slug) {
+      setSlug(false);
+      setCuid(newCuid());
+    } else {
+      setSlug(true);
+      setCuid(newCuid.slug());
+    }
+  };
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>CUID Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="https://github.com/ericelliott/cuid">CUID</a> Generator
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Use these cuids at your own risk!
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
-            </p>
-          </a>
+        <div>
+          <h2>{cuid}</h2>
+        </div>
+        <div>
+          <button onClick={refreshCuid}>Refresh</button>
+          <button onClick={toggleSlug}>Toggle Slugs</button>
         </div>
       </main>
 
@@ -200,5 +202,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
